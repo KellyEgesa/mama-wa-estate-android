301fb7004cd85f88b65b65902d6ae158620d9bdb
KellyEgesa <kelly.egesa@gmail.com>
2021-02-12 12:24:08 +0300

    Made changes

--- app/src/main/java/com/mamawaestate/android/CheckOutActivity.java
+++ app/src/main/java/com/mamawaestate/android/CheckOutActivity.java
@@ -4,11 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity2 extends AppCompatActivity {
+public class CheckOutActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_checkout);
+
     }
 }
