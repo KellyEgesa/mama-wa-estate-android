107fdcc2ccafe0573a88f0a93b507bf94ba9e893
Francisndungu2 <francianmbugua@gmail.com>
2021-02-17 06:44:03 +0300

    modified

--- app/src/main/java/com/mamawaestate/android/MainActivity.java
+++ app/src/main/java/com/mamawaestate/android/MainActivity.java
@@ -12,22 +12,13 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 
 public class MainActivity extends AppCompatActivity {
-    ActionBarDrawerToggle actionBarDrawerToggle;
-    DrawerLayout drawerLayout;
-    Toolbar toolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        drawerLayout = findViewById(R.id.drawer_layout);
-
-        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
-                R.string.drawer_open, R.string.drawer_close);
-
-        drawerLayout.addDrawerListener(actionBarDrawerToggle);
 
 
     }}
