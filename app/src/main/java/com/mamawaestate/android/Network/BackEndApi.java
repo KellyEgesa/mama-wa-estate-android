e590ef8613c41f6c797d7f2d6bce520d93ec4fa6
luciahroyalty101 <luciahkei101@gmail.com>
2021-02-27 20:28:27 +0300

    modified files

--- app/src/main/java/com/mamawaestate/android/Network/BackEndApi.java
+++ app/src/main/java/com/mamawaestate/android/Network/BackEndApi.java
@@ -1,12 +1,15 @@
 package com.mamawaestate.android.Network;
 
+import com.mamawaestate.android.models.Products;
 import com.mamawaestate.android.models.UserData;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Query;
 
-public interface BackEndApi {
+public interface BackEndApi<slug, category, image> {
     @POST("/api/auth/register")
     Call<UserData> registerVendor(@Body UserData userData);
     @POST("/api/auth/login")
@@ -15,4 +18,20 @@ public interface BackEndApi {
     Call<UserData> registerConsumer(@Body UserData userData);
     @POST("/api/v1/login/")
     Call<UserData> loginUser(@Body UserData userData);
+//    @GET("/api/v1/products")
+//    call("everything") products(@Body Products products)
+//    @GET("everything")
+//    Call<Products> getProducts(
+//            @Query("tittle") String tittle
+//             @Query("Slug") String slug
+//            @Query("Image") String image
+//                     @Query("Category") String category
+//                     @Query("markedPrice") int markedPrice
+//            @Query("sellingPrice") int sellingPrice
+//            @Query("Description") String description
+//            @Query("Warranty") String warranty
+//            @Query("returnPolicy") String returnPolicy
+//    );
+
+
 }
